3c8f93ef3df3cd4b6a039a6a0c86c09d9424b08c
Amanda Muniz <amanda.muniiz@outlook.com>
2018-11-05 10:33:36 -0200

    #71 Changing user to auth user and fixing issues

--- usuario/models.py
+++ usuario/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-name_length = 50
-email_length = 150
-senha_length = 10
+length = 5000
 
 class Usuario(models.Model):
-    nome =  models.CharField(max_length = name_length, blank = True)
-    email = models.CharField(max_length = email_length, blank = True)
-    senha = models.CharField(max_length = senha_length, blank = True)
-
+    usuario = models.ForeignKey(
+        User,
+        related_name = 'User',
+        null = True,
+        on_delete = models.CASCADE
+    )
+    tokenUsuario = models.TextField(
+        max_length = length,
+        default = None,
+        blank = True,
+        null=True
+    )
     # Campo que recebe a ForeignKey de avaliacao
     # Tirar da forma de comentario quando avaliacao já existir.
     # avaliacao = models.ForeignKey(
