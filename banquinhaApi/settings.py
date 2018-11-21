010895b715eda65bc986391bdca854c21f015e66
Amanda Muniz <amanda.muniiz@outlook.com>
2018-11-20 23:50:08 -0200

    #71 Fixing minors errors
    Signed-off-by: Amanda Muniz <amanda.muniiz@outlook.com>
    Co-authored-by: Isaque Alves <isaquealvesdl@gmail.com>

--- banquinhaApi/settings.py
+++ banquinhaApi/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True   
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'banquinhaApi.urls'
 
